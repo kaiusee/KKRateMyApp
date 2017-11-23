2aac3ebfb167274084e1c60ab85468c509314019
Kais K <kaiusee@gmail.com>
2017-11-23 00:15:26 -0500

    Update KKRateMyApp.swift
    Updated to use SKStoreReviewController

--- KKRateMyApp.swift
+++ KKRateMyApp.swift
@@ -7,53 +7,22 @@
 //
 
 import UIKit
+import StoreKit
 
 class KKRateMyApp {
-
+    
     static let sharedInstance = KKRateMyApp()
-    
-    var appName: String = ""
-    var appId: String = ""
-    var numberOfTimesBeforePrompts: Int = 0
-    
-    func rateApp() {
+    var numberOfTimesBeforePrompts: Int = 5
+    private let userDefaultsKey = "kkRateAppUsageCount"
 
-        if appName == "" {
-            print("Error: App Name needs to be set")
-            return
-        }
-        
-        if appId == "" {
-            print("Error: App ID needs to be set")
-            return
-        }
-        
-        if numberOfTimesBeforePrompts == 0 {
-            print("Error: NumberOfTimesBeforePrompts needs to be set")
-            return
-        }
-        
-        let userDefaultsKey = "kkRateAppUsageCount"
+    func rateApp() {
         
         var usageCount: Int = UserDefaults.standard.integer(forKey: userDefaultsKey)
         if usageCount > 0 {
             usageCount += 1
             UserDefaults.standard.set(usageCount, forKey: userDefaultsKey)
             if usageCount == self.numberOfTimesBeforePrompts {
-                
-                let alertController = UIAlertController(title: "Rate \(self.appName)", message: "If you enjoy using \(self.appName), would you mind taking a moment to rate it?\nThanks for your support!", preferredStyle: .alert)
-                
-                let OKAction = UIAlertAction(title: "Sure!", style: .default) { action in
-                    if let appStoreURL = URL(string: "itms-apps://itunes.apple.com/app/id\(self.appId)") {
-                        UIApplication.shared.open(appStoreURL)
-                    }
-                }
-                
-                let cancelAction = UIAlertAction(title: "No, Thanks!", style: .cancel)
-                alertController.addAction(cancelAction)
-                
-                alertController.addAction(OKAction)
-                UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true)
+                SKStoreReviewController.requestReview()
             }
         }
         else {
@@ -61,3 +30,4 @@ class KKRateMyApp {
         }
     }
 }
+
